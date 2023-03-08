ecf56e7d4ed71bdb6264b3f9ed2474f93e4cdd04
hwanguebest@naver.com <hwanguebest@naver.com>
2023-03-08 17:30:39 +0900

    Update: 프로젝트 추가

---
+++ backend/incense/src/main/java/com/suyang/incense/IncenseApplication.java
@@ -0,0 +1,13 @@
+package com.suyang.incense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncenseApplication.class, args);
+	}
+
+}
