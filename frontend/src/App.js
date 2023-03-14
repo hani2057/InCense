0b4708777a8e856022c99e2744d41068e648f558
sinecure96 <sinecure96@gmail.com>
2023-03-14 15:32:28 +0900

    [S08P22A804-139] Update: 향수리스트 페이지 틀 제작

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router";
 import LogInPage from "./pages/LogInPage/LogInPage";
+import ListPage from "./pages/ListPage/ListPage";
+import DetailPage from "./pages/DetailPage/DetailPage";
+
 
 function App() {
   return (
     <Routes>
       {/* <Route index element={<MainPage />} /> */}
       <Route path="/login" element={<LogInPage />} />
+      <Route path="/list" element={<ListPage />} />
+      <Route path="/detail" element={<DetailPage />} />
+
     </Routes>
   );
 }
