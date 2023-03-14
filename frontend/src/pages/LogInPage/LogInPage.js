15c831f09e0c2c3c6be58b3d12d5b5227fdb89a3
hani <hanikim2057@gmail.com>
2023-03-14 12:32:16 +0900

    [S08P22A804-90] Update: 회원가입페이지 CSS 완료

--- frontend/src/pages/LogInPage/LogInPage.js
+++ frontend/src/pages/LogInPage/LogInPage.js
@@ -5,11 +5,17 @@ import { TitleSpan, LoginBtn } from "./style";
 const LogInPage = () => {
   return (
     <FlexDiv height="100vh">
-      <FlexDiv direction="column" justify="center">
+      <FlexDiv
+        direction="column"
+        justify="center"
+        width="45%"
+        style={{ backgroundImage: "url('/assets/images/bgimg1.png')" }}
+      >
         <TitleSpan>In</TitleSpan>
         <TitleSpan>Cense</TitleSpan>
       </FlexDiv>
-      <FlexDiv direction="column">
+      <FlexDiv direction="column" gap="1rem">
+        <span style={{ fontSize: "2rem", marginBottom: "4rem" }}>Log In</span>
         <LoginBtn>KaKao</LoginBtn>
         <LoginBtn>Naver</LoginBtn>
         <LoginBtn>Google</LoginBtn>
