16a879e1b692ccbd5d1e2c606b514b2abf0f86f4
sinecure96 <sinecure96@gmail.com>
2023-03-16 14:53:35 +0900

    [S08P22A804-145] Update: 향수 디테일 페이지 틀/기능 개발

---
+++ frontend/src/components/common/DivideLine/DivideLine.js
@@ -0,0 +1,10 @@
+import { Box } from "@mui/system"
+
+
+export default function DivideLine() {
+  return(
+    <Box
+      sx={{width:'80rem', height:'0.2rem', backgroundColor:'#DCDCDC',marginTop:'2rem',marginBottom:'2rem'}}>
+    </Box>
+  )
+} 
