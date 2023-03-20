eed246baa90b29d854f259a6ae07461a89349c0e
hani <hanikim2057@gmail.com>
2023-03-20 14:08:09 +0900

    [S08P22A804-198] Update: 내비게이션 바 기본 구조 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,21 @@ import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import ListPage from "./pages/ListPage/ListPage";
 import DetailPage from "./pages/DetailPage/DetailPage";
 import SharePage from "./pages/SharePage/SharePage";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<MainPage />} />
-      <Route path="/login" element={<LogInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/list" element={<ListPage />} />
-      <Route path="/detail" element={<DetailPage />} />
-      <Route path="/share" element={<SharePage />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/share" element={<SharePage />} />
+      </Routes>
+    </>
   );
 }
 
