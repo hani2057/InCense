bb0d00966a57245e9f1ce8e25b73e350b47c390a
heeralce <heeracle@naver.com>
2023-03-20 17:50:52 +0900

    Update: 판매 나눔] 조회 더미 작성중

---
+++ backend/incense/src/main/java/com/suyang/incense/api/response/deal/DealDetailRes.java
@@ -0,0 +1,11 @@
+package com.suyang.incense.api.response.deal;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ApiModel("DealDetailRes")
+public class DealDetailRes {
+}
