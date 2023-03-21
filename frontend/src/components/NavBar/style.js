9fc643b1268fc4b2aed1d6e897b3f0d82a0c2e80
hani <hanikim2057@gmail.com>
2023-03-21 15:14:29 +0900

    [S08P22A804-191] Update: 프로필인포 기본 CSS 구현

--- frontend/src/components/NavBar/style.js
+++ frontend/src/components/NavBar/style.js
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { FlexDiv } from "../common/FlexDiv/FlexDiv";
 
 const NavWrapper = styled(FlexDiv)`
