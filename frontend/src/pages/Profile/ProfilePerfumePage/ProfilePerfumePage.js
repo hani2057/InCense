d232836ec6f97947f7261dae876dd97764d7aa1a
hani <hanikim2057@gmail.com>
2023-03-21 17:09:42 +0900

    [S08P22A804-193] Update: 프로필 후기페이지 헤더 레이아웃 구현

--- frontend/src/pages/Profile/ProfilePerfumePage/ProfilePerfumePage.js
+++ frontend/src/pages/Profile/ProfilePerfumePage/ProfilePerfumePage.js
@@ -17,9 +17,23 @@ const ProfilePerfumes = () => {
           width="50%"
           padding="0 5%"
         />
-        <FlexDiv width="50%">
-          <img src="/assets/icons/search.svg" alt="search" />
-          <span>목록에 향수 추가</span>
+        <FlexDiv width="50%" padding="0 5%">
+          <FlexDiv justify="start">
+            <img src="/assets/icons/search.svg" alt="search" />
+            <span>목록에 향수 추가</span>
+          </FlexDiv>
+          <FlexDiv direction="column" align="end">
+            <span>내 취향에 반영하기</span>
+            <span
+              style={{
+                fontSize: "0.625rem",
+                position: "relative",
+                top: "0.5rem",
+              }}
+            >
+              마지막 반영일자 Feb 10, 2023
+            </span>
+          </FlexDiv>
         </FlexDiv>
       </ProfilePerfumeHeader>
       <div style={{ height: "150px" }}></div>
