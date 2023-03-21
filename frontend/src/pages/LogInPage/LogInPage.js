a64daf2c305d06165a660953247881e8397ff891
hani <hanikim2057@gmail.com>
2023-03-21 16:34:56 +0900

    [S08P22A804-194] Update: 카카오로그인 인가코드 백으로 전달 api 구현

--- frontend/src/pages/LogInPage/LogInPage.js
+++ frontend/src/pages/LogInPage/LogInPage.js
@@ -4,7 +4,7 @@ import { FlexDiv } from "../../components/common/FlexDiv/FlexDiv";
 import { TitleSpan, LoginBtn } from "./style";
 
 const CLIENT_ID = `885f85b9b3a981a7a2a73071720fb28e`;
-const REDIRECT_URI = `http://localhost:3000/signup`;
+const REDIRECT_URI = `http://localhost:3000/oauth/callback/kakao`;
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize`;
 
