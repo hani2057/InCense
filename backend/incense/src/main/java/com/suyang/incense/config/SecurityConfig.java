e10d2df083cdc53c1f616d8cf2cad0b0a495cb92
yurim <yurimkang20@gmail.com>
2023-03-23 16:57:48 +0900

    [S08P22A804-154] Update: SecurityConfig.java 수정

--- backend/incense/src/main/java/com/suyang/incense/config/SecurityConfig.java
+++ backend/incense/src/main/java/com/suyang/incense/config/SecurityConfig.java
@@ -1,20 +1,20 @@
 package com.suyang.incense.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
                 .csrf().disable()
@@ -24,5 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/**").permitAll()
                 .anyRequest().permitAll()
                 .and().cors();
+        return http.build();
     }
 }
