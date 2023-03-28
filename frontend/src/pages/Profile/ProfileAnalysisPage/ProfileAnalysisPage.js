ff7928b75ebd4dd61b7302500e569d685bf49d38
hani <hanikim2057@gmail.com>
2023-03-28 10:03:59 +0900

    [S08P22A804-93] Update: 가로스크롤 컨테이너 컴포넌트 생성

--- frontend/src/pages/Profile/ProfileAnalysisPage/ProfileAnalysisPage.js
+++ frontend/src/pages/Profile/ProfileAnalysisPage/ProfileAnalysisPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlexDiv } from "../../../components/common/FlexDiv/FlexDiv";
+import { ScrollContainer } from "../../../components/common/ScrollContainer/ScrollContainer";
 import ProfileTitleBox from "../../../components/Profile/ProfileTitleBox/ProfileTitleBox";
 import WantAnalysisCard from "../../../components/Profile/WantAnalysisCard/WantAnalysisCard";
 import { ProfileOutletContainer } from "../ProfilePage/style";
@@ -45,10 +46,8 @@ const ProfileAnalysisPage = () => {
       <FlexDiv direction="column">
         <ProfileTitleBox bgimgNo={1} title={`${name}님의 취향 분석 결과`} />
         <ProfileTitleBox bgimgNo={1} title={"I want it 향수 취향 적중도"} />
-        <FlexDiv
-          justify="start"
-          style={{ overflowX: "scroll", paddingBottom: "10rem" }}
-        >
+
+        <ScrollContainer margin="0 0 10rem 0">
           {iWantItList.map(({ name, brand, img }, idx) => (
             <WantAnalysisCard
               name={name}
@@ -58,7 +57,8 @@ const ProfileAnalysisPage = () => {
               key={idx}
             />
           ))}
-        </FlexDiv>
+        </ScrollContainer>
+
         <ProfileTitleBox
           bgimgNo={1}
           title={`${name}님의 취향 기반 추천 향수`}
