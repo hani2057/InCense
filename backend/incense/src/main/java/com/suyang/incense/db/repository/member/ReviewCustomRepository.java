94400208850ddd6dd2c04901f1cb7d464b1c5314
yurim <yurimkang20@gmail.com>
2023-03-30 16:41:54 +0900

    [S08P22A804-245] Update: 후기 조회 구현 완료

--- backend/incense/src/main/java/com/suyang/incense/db/repository/member/ReviewCustomRepository.java
+++ backend/incense/src/main/java/com/suyang/incense/db/repository/member/ReviewCustomRepository.java
@@ -1,12 +1,16 @@
 package com.suyang.incense.db.repository.member;
 
+import com.suyang.incense.api.response.member.mypage.ReviewRes;
 import com.suyang.incense.db.entity.member.Member;
 import com.suyang.incense.db.entity.perfume.Perfume;
 import com.suyang.incense.db.entity.review.Review;
 
+import java.util.List;
 
 
 public interface ReviewCustomRepository {
 
     Review getReviewByMemberAndPerfume(Member member, Perfume perfume);
+
+    List<ReviewRes> getReviewByMember(Member member);
 }
--- backend/incense/src/main/java/com/suyang/incense/db/repository/member/ReviewCustomRepositoryImpl.java
+++ backend/incense/src/main/java/com/suyang/incense/db/repository/member/ReviewCustomRepositoryImpl.java
@@ -1,12 +1,18 @@
 package com.suyang.incense.db.repository.member;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.suyang.incense.api.response.member.mypage.ReviewRes;
 import com.suyang.incense.db.entity.member.Member;
 import com.suyang.incense.db.entity.perfume.Perfume;
 import com.suyang.incense.db.entity.review.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static com.suyang.incense.db.entity.perfume.QBrand.brand;
+import static com.suyang.incense.db.entity.perfume.QPerfume.perfume;
 import static com.suyang.incense.db.entity.review.QReview.review;
 
 @Repository
@@ -21,6 +27,23 @@ public class ReviewCustomRepositoryImpl implements ReviewCustomRepository {
                 .selectFrom(review)
                 .where(review.member.eq(member), review.perfume.eq(perfume))
                 .fetchOne();
+    }
+
+    @Override
+    public List<ReviewRes> getReviewByMember(Member member) {
+        return jpaQueryFactory
+                .select(Projections.constructor(
+                        ReviewRes.class,
+                        perfume.id,
+                        brand.name,
+                        perfume.name,
+                        review.preference,
+                        review.comment,
+                        review.modifiedDate
+                ))
+                .from(perfume, brand, review)
+                .where(review.member.eq(member), review.perfume.eq(perfume), perfume.brand.eq(brand))
+                .fetch();
     }
 
 }
