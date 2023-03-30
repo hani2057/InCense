4ff86db840dac7608f281ec988fcfd6740376883
yurim <yurimkang20@gmail.com>
2023-03-30 13:12:49 +0900

    [S08P22A804-252] Update: Have,Had,Want 정보 변경 구현 완료

--- backend/incense/src/main/java/com/suyang/incense/api/service/member/MyPageService.java
+++ backend/incense/src/main/java/com/suyang/incense/api/service/member/MyPageService.java
@@ -1,5 +1,6 @@
 package com.suyang.incense.api.service.member;
 
+import com.suyang.incense.api.request.member.mypage.PerfumeModifyReq;
 import com.suyang.incense.api.request.member.mypage.PerfumeRegisterReq;
 import com.suyang.incense.api.response.member.mypage.PerfumeRes;
 import org.springframework.security.core.Authentication;
@@ -10,5 +11,6 @@ public interface MyPageService {
 
     List<PerfumeRes> getMyPerfume(String type, Authentication Authentication);
     void registerPerfume(PerfumeRegisterReq perfumeRegisterReq, Authentication authentication);
+    void modifyPerfume(PerfumeModifyReq perfumeModifyReq);
     void removePerfume(Long myPerfumeId);
 }
