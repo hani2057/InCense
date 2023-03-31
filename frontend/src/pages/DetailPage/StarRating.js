d5ee383581a4a3d82943a3d27e136030694bad7f
hani <hanikim2057@gmail.com>
2023-03-31 11:00:40 +0900

    [S08P22A804-220] Update: StarRating 컴포넌트 사이즈 프롭스 전달

--- frontend/src/pages/DetailPage/StarRating.js
+++ frontend/src/pages/DetailPage/StarRating.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import './StarRating.css'
+import React from "react";
+import "./StarRating.css";
 
-function StarRating() {
-
+function StarRating({ size }) {
   const drawStar = (e) => {
-    document.querySelector(`.star span`).style.width = `${e.target.value * 10}%`;
-    console.log(e.target.value)
-  }
+    document.querySelector(`.star span`).style.width = `${
+      e.target.value * 10
+    }%`;
+    console.log(e.target.value);
+  };
 
   return (
     <div>
-      <span className="star">
+      <span className="star" style={{ fontSize: `${size || "2rem"}` }}>
         ★★★★★
         <span>★★★★★</span>
-        <input type="range" onInput={drawStar} value="1" step="1" min="0" max="10"/>
+        <input
+          type="range"
+          onInput={drawStar}
+          value="1"
+          step="1"
+          min="0"
+          max="10"
+        />
       </span>
     </div>
-  )
+  );
 }
 
-export default StarRating
+export default StarRating;
