efdef57c313fc9ee1bc15db6f9b68531de21c2c4
heeralce <heeracle@naver.com>
2023-03-31 17:25:26 +0900

    Update: perfume test

--- backend/incense/src/main/java/com/suyang/incense/api/service/test/TestService.java
+++ backend/incense/src/main/java/com/suyang/incense/api/service/test/TestService.java
@@ -1,4 +1,13 @@
 package com.suyang.incense.api.service.test;
 
+import com.suyang.incense.api.request.test.TestResultDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface TestService {
+
+    ResponseEntity<TestResultDto> getPreferenceData(String url, List<Integer> testAnswer);
+
+    boolean savePreference(Long memberId, TestResultDto testResultDto);
 }
--- backend/incense/src/main/java/com/suyang/incense/api/service/test/TestServiceImpl.java
+++ backend/incense/src/main/java/com/suyang/incense/api/service/test/TestServiceImpl.java
@@ -1,8 +1,39 @@
 package com.suyang.incense.api.service.test;
 
 
+import com.suyang.incense.api.request.test.TestResultDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class TestServiceImpl implements TestService{
+
+    public ResponseEntity<TestResultDto> getPreferenceData(String url, List<Integer> testAnswer) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        //Header set
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        //Body set : 사용자로부터 받은 테스트 데이터
+        MultiValueMap<String, List<Integer>> body = new LinkedMultiValueMap<>();
+        body.add("choose", testAnswer);
+
+        //Message
+        HttpEntity<?> requestMessage = new HttpEntity<>(body, httpHeaders);
+
+        //Request
+        ResponseEntity<TestResultDto> response = restTemplate.postForEntity(url, requestMessage, TestResultDto.class);
+
+        return response;
+    }
 }
