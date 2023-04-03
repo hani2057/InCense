e56c06b97ab0f1cd7ebfa459c21a0b649486bf6a
hwanguebest@naver.com <hwanguebest@naver.com>
2023-04-03 14:31:27 +0900

    Update: websocket component 추가

---
+++ frontend/src/components/WebSocket/index.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+const WebSocket = ()=>{
+    const dispatch = useDispatch();
+
+    useEffect( ()=>{
+        dispatch({ type: "START_WEBSOCKET" });
+
+        return dispatch({type: "CLOSE_SOCKET"})
+    },[])
+
+    return <></>
+
+}
+
+
+export default WebSocket
