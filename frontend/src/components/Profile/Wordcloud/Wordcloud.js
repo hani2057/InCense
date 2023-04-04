41695a3b21955459792489520154de00b648615b
hani <hanikim2057@gmail.com>
2023-04-04 17:13:40 +0900

    [S08P22A804-273] Update: 워드클라우드 라이브러리 제거

--- frontend/src/components/Profile/Wordcloud/Wordcloud.js
+++ frontend/src/components/Profile/Wordcloud/Wordcloud.js
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactWordcloud from "react-wordcloud";
+// import React from "react";
+// import ReactWordcloud from "react-wordcloud";
 
-const Wordcloud = () => {
-  const words = [
-    { text: "text1", value: 40 },
-    { text: "text2", value: 64 },
-    { text: "text3", value: 16 },
-    { text: "text4", value: 11 },
-    { text: "text5", value: 80 },
-    { text: "text6", value: 25 },
-    { text: "text7", value: 32 },
-  ];
+// const Wordcloud = () => {
+//   const words = [
+//     { text: "text1", value: 40 },
+//     { text: "text2", value: 64 },
+//     { text: "text3", value: 16 },
+//     { text: "text4", value: 11 },
+//     { text: "text5", value: 80 },
+//     { text: "text6", value: 25 },
+//     { text: "text7", value: 32 },
+//   ];
 
-  return (
-    <div>
-      <ReactWordcloud
-        words={words}
-        options={{ rotationAngles: [0, -90], rotations: 2 }}
-      />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <ReactWordcloud
+//         words={words}
+//         options={{ rotationAngles: [0, -90], rotations: 2 }}
+//       />
+//     </div>
+//   );
+// };
 
-export default Wordcloud;
+// export default Wordcloud;
