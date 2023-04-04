e4c8e561c650457d6fa822fdcc57f0d9b5d1a9da
hani <hanikim2057@gmail.com>
2023-04-04 09:04:53 +0900

    [S08P22A804-278] Fix: 로그인 클라이언트키 원복

--- frontend/src/pages/LogInPage/LogInPage.js
+++ frontend/src/pages/LogInPage/LogInPage.js
@@ -2,13 +2,13 @@ import React from "react";
 import { FlexDiv } from "../../components/common/FlexDiv/FlexDiv";
 import { TitleSpan, LoginBtn } from "./style";
 
-const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
+const KAKAO_CLIENT_ID = "219f3d4b6069d9e1cbc39012ba719f67";
 const KAKAO_REDIRECT_URI = `https://j8a804.p.ssafy.io/oauth/callback/kakao`;
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 const KAKAO_LOCAL_REDIRECT_URI = `http://localhost:3000/oauth/callback/kakao`;
 const KAKAO_LOCAL_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_LOCAL_REDIRECT_URI}&response_type=code`;
 
-const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
+const NAVER_CLIENT_ID = "N3pZgno1b_CSURFjx9Yw";
 const NAVER_REDIRECT_URI = `https://j8a804.p.ssafy.io/oauth/callback/naver`;
 const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=1234`;
 
