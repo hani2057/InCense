7d31df699fc1457050fd50a4252635dff55c8ac2
hwanguebest@naver.com <hwanguebest@naver.com>
2023-04-06 17:06:58 +0900

    UPdate: change

--- frontend/src/sagas/alarmsaga.js
+++ frontend/src/sagas/alarmsaga.js
@@ -3,14 +3,12 @@
 import { take, call, put, fork, cancel, takeLatest } from "redux-saga/effects";
 
 import api from "../apis/api"
-import { selectAlarmLen } from "../store/slice/alarmSlice";
 
 function* alarmRun() {
     yield call(api.alarm.readAlarmSendAll());
     const res = yield call(api.alarm.getAlarmSend);
     yield put({type:"alarm/selectAlarmList",payload:res})
     yield put({type: "alarm/increaseAlarmCount"});
-    yield put({type: "alarm/setAlarm",payload:selectAlarmLen})
   }
 
 
